7bbc42bf29bf52e845709fdc0f1aca6035d522c7
Mezan2002 <mezanurr009@gmail.com>
2022-12-24 01:23:42 +0600

    Home design done and all fields are requires

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,88 +2,79 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
+  const handleSave = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
-      {/* <div className="card neomorphic mt-12 w-4/12 mx-auto shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">
-            <p className="text-center mb-10 text-2xl">
-              Please enter your name and pick the Sectors you are currently
-              involved in.
-            </p>
-          </h2>
-          <div>
-            <div className="form-control w-full">
-              <label className="label">
-                <span className="label-text">Your Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full"
-              />
-            </div>
-          </div>
-          <div className="card-actions">
-            <button className="btn btn-primary btn-block">Save</button>
-          </div>
-        </div>
-      </div> */}
-      <div className="hero min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="w-1/2">
+      <div className="hero min-h-screen my-10 md:my-0">
+        <div className="hero-content flex-col-reverse items-center md:flex-row-reverse md:px-5">
+          <div className="md:w-1/2">
             <div className="card neomorphic mx-auto shadow-xl">
-              <div className="card-body">
+              <form onSubmit={handleSave} className="card-body">
                 <h2 className="card-title">
-                  <p className="text-center mb-10 text-2xl">
+                  <p className="text-center mb-10 text-xl">
                     Please enter your name and pick the Sectors you are
                     currently involved in.
                   </p>
                 </h2>
-                <div>
+                <div className="mb-5">
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Your Name</span>
                     </label>
                     <input
                       type="text"
-                      placeholder="Type here"
+                      placeholder="Your Name"
                       className="input input-bordered w-full"
+                      required
                     />
                   </div>
-                  <div className="form-control w-full max-w-xs">
+                  <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Sectors</span>
                     </label>
-                    <select className="select select-bordered">
+                    <select className="select select-bordered" required>
                       <option disabled selected>
-                        Pick one
+                        Select your sector
                       </option>
-                      <option>Star Wars</option>
-                      <option>Harry Potter</option>
+                      <option>Manufacturing</option>
+                      <option>
+                        &nbsp;&nbsp;&nbsp;&nbsp;Construction materials
+                      </option>
                       <option>Lord of the Rings</option>
                       <option>Planet of the Apes</option>
                       <option>Star Trek</option>
                     </select>
                   </div>
+                  <div className="form-control mt-5">
+                    <label className="flex items-center cursor-pointer w-[135px]">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-primary mr-3"
+                        required
+                      />
+                      <span className="label-text">Agree to terms</span>
+                    </label>
+                  </div>
                 </div>
                 <div className="card-actions">
                   <button className="btn btn-primary btn-block">Save</button>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
-          <div className="w-1/2 mt-[-400px] relative">
-            <h1 className="text-4xl text-primary font-bold capitalize">
+          <div className="md:w-1/2 lg:mt-[-380px] md:relative md:text-left text-center">
+            <h1 className="lg:text-4xl md:text-2xl text-3xl text-primary font-bold capitalize">
               Welcome to our community
             </h1>
-            <p className="py-3">
-              Provide your name and occupation for next action
+            <p className="py-3 w-8/12 md:w-full mx-auto">
+              Provide your name and your working sector and follow us
             </p>
             <img
               src="https://i.ibb.co/HDSFB0v/Screenshot-2022-12-23-233201-removebg-preview.png"
               alt=""
-              className="absolute top-0 right-0"
+              className="lg:absolute top-0 right-0"
             />
           </div>
         </div>
